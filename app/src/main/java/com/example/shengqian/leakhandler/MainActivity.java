4050f35d679cb9da1f72205f51e58b3ab594b178
shengqian <shengqian@szsoling.com>
2017-12-26 15:53:17 +0800

    弱引用

--- app/src/main/java/com/example/shengqian/leakhandler/MainActivity.java
+++ app/src/main/java/com/example/shengqian/leakhandler/MainActivity.java
@@ -5,6 +5,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import java.lang.ref.WeakReference;
+
 public class MainActivity extends AppCompatActivity {
 
     private Handler mHandler = new Handler();
@@ -23,15 +25,18 @@ public class MainActivity extends AppCompatActivity {
      * 静态内部类
      */
     private static class DoRunAble implements Runnable {
-        private TextView mTextView;
+        private WeakReference<TextView> mTextView;
 
         public DoRunAble(TextView textView) {
-            mTextView = textView;
+            mTextView = new WeakReference<TextView>(textView);
         }
 
         @Override
         public void run() {
-            mTextView.setText("十年后..");
+            TextView textView = mTextView.get();
+            if (textView != null) {
+                textView.setText("十年后..");
+            }
         }
     }
 
