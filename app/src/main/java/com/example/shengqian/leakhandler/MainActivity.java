0c5a65ec96c6b746e2bc5943287974793c0cf887
shengqian <shengqian@szsoling.com>
2017-12-27 15:20:21 +0800

    GC验证

--- app/src/main/java/com/example/shengqian/leakhandler/MainActivity.java
+++ app/src/main/java/com/example/shengqian/leakhandler/MainActivity.java
@@ -1,24 +1,46 @@
 package com.example.shengqian.leakhandler;
 
+import android.content.Intent;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import java.lang.ref.WeakReference;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private Handler mHandler = new Handler();
     private TextView mHelloTV;
+    private Button mJumpBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mHelloTV = (TextView) findViewById(R.id.tv_hello);
+        mJumpBtn = (Button) findViewById(R.id.btn_Jump);
+        mJumpBtn.setOnClickListener(this);
 
         mHandler.postDelayed(new DoRunAble(mHelloTV), 15000);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_Jump:
+                Intent intent = new Intent();
+                intent.setClass(MainActivity.this, SecondActivity.class);
+                startActivity(intent);
+                break;
+
+            default:
+                break;
+        }
+
+
     }
 
     /**
